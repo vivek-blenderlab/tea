0b2547076a3d517166887f1834d31275dcea1023
vivek-blenderlab <blenderlab24@gmail.com>
2026-01-18 18:54:13 +0530

    waste deleted

--- edu.py
+++ edu.py
@@ -1,10 +1,28 @@
 # ---imports
+import streamlit as st
+
+# ---user details 
+user = st.sidebar.text_input("enter the your name:")
+
+
+
 
 # ---slidebar (user information)
+Sidebar = st.sidebar.write(f"wlelcome to the journey , {user}")
+add = st.sidebar.button("add study material here")
+if add :
+    material = st.sidebar.text_input("material")
 
-# ---make 2 column 
 
-# ---col1 select what to study (multi seslect)
+# ---make 2 column  
+col1,col2 = st.columns(2)
+
+# ---col1 select what to study (multi seslect) & day planner
+with col1:
+    Selection_box = st.selectbox("choose what to study today :" , ["maths","physic","english","chemistry","Biology","analog circuit","graphics","cad designing"])
+
+
+
 # ---col1 available study templete 
 #           0.study all
 #           1.pdf summery
