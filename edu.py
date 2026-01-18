8d9475a6bba2bb4dd7c98b2955f3555a05f9b982
vivek-blenderlab <blenderlab24@gmail.com>
2026-01-18 18:48:42 +0530

    dev created

---
+++ edu.py
@@ -0,0 +1,21 @@
+# ---imports
+
+# ---slidebar (user information)
+
+# ---make 2 column 
+
+# ---col1 select what to study (multi seslect)
+# ---col1 available study templete 
+#           0.study all
+#           1.pdf summery
+#           2.video summary
+#           3.web scrapper 
+#           4.flash cards 
+#           5.simple search 
+#           6.question bank 
+#           7.more knowledge 
+#           8.prarical/industrial use case 
+
+
+
+# ---col2 output 
