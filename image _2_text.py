55b9a2fe54b0ac75a7a0ca830b204ff1cb5f1b31
vivek-blenderlab <blenderlab24@gmail.com>
2026-01-23 11:36:45 +0530

    Implement image reading with language parameter
    Added a function to read images with language support and outlined data saving structure.

--- image _2_text.py
+++ image _2_text.py
@@ -6,3 +6,33 @@ reader = easyocr.Reader(["en"])
 result = reader.readtext("photos\shayri.jpeg")
 
 print(result)
+
+
+
+#### edit this
+"""
+batch_size (int, default = 1) - batch_size>1 will make EasyOCR faster but use more memory
+
+def read_img(sta_path , lang) :
+  reader = easyocr.Reader(lang)
+  result = reader.readtext(sta_path , details = 0 , gpu = False
+  return result , reader 
+  
+  
+# save extracted data into another new dir user_data/exe_data
+exe_data:
+            img
+            pdf
+            video transcript
+            convo summery
+            user_plan(summery as text)
+
+
+# pdf summery 
+
+
+
+
+
+            
+"""
