a739c32b03ca6abf327a7169f38c231506f5a02c
vivek-blenderlab <blenderlab24@gmail.com>
2026-02-10 13:07:31 +0530

    moved yml file for codespace

--- edu.py
+++ edu.py
@@ -1,5 +1,24 @@
 # ---imports
 import streamlit as st
+from dotenv import load_dotenv
+import study_temp.pdf_summary 
+import study_temp.img_2_txt
+import study_temp.chat_agent
+import study_temp.flash_cards
+import study_temp.video_tran
+import study_temp.convo_summery
+import study_temp.user_plan
+import os
+
+
+from langchain_openai import ChatOpenAI
+
+
+
+
+load_dotenv()  # reads .env file
+
+
 
 # ---user details 
 user = st.sidebar.text_input("enter the your name:")
